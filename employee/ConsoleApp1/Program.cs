8a43225e8172dd576d3bb9cc67bd94628f6a70a1
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-09-24 15:58:49 +1200

    Add files via upload

---
+++ employee/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        public int employeeID { get; set; }
+        string fname, lname, salary, tax;
+        public string fName
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+        public string lName
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public int salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public int tax
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+        public Employee(string _fname, string _lname, string _salary, string _tax)
+        {
+            fName = _fname;
+            lName = _lname;
+            salary = _salary;
+            tax = _tax;
+        }
+        public string NetSalary()
+        {
+        
+        }
+
+    }
+    
+}
